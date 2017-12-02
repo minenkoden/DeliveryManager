6805b1800080c39b40ad4a9d872fd4a1863340d2
minenko <minenko.den@gmail.com>
2017-12-02 18:22:45 +0200

    Custom attribute for categories

---
+++ app/code/MD/DeliveryManager/Setup/InstallSchema.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author Minenko Denys
+ */
+namespace MD\DeliveryManager\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'delivery_manager_email',
+            [
+                'type' => 'text',
+                'label' => 'Delivery Manager Email',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 4,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'wysiwyg_enabled' => true,
+                'is_html_allowed_on_front' => true,
+                'group' => 'General Information',
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
---
+++ app/code/MD/DeliveryManager/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Minenko Denys
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MD_DeliveryManager',
+    __DIR__
+);
